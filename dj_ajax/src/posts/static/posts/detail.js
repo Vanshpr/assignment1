655e936a7613799b0c559ce8fc80c855bb98779a
Vansh Prajapati <vansh.prajapati1104@gmail.com>
2024-04-06 16:05:45 -0400

    user can update and delete there posts only logic

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -3,6 +3,8 @@ console.log("...hello detial js sctztic page...")
 const backBtn = document.getElementById('back-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
+const updateBtn = document.getElementById('update-btn')
+const deleteBtn = document.getElementById('delete-btn')
 
 backBtn.addEventListener('click', ()=>{
     history.back()
@@ -12,7 +14,17 @@ $.ajax({
     type: 'GET',
     url: url,
     success: function(response){
-        console.log(response)
+        const data = response.data
+
+        if (data.logged_in !== data.author){
+            console.log('Different')
+        }else{
+            console.log('The same')
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+
+
         spinnerBox.classList.add('not-visible')
     },
     error: function(error){
