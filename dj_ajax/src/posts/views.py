73747812b512e29b5fcc3aa7b2475e7297af75f0
Vansh Prajapati <vansh.prajapati1104@gmail.com>
2024-04-06 04:02:43 -0400

    first view and first template

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Post
 
 # Create your views here.
+def post_list_and_create(request):
+  qs = Post.objects.all()
+  return render(request, 'posts/main.html', {'qs':qs})
