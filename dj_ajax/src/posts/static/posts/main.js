29979ed2cb4ad1aec61daf25d4b662af75f9c855
Vansh Prajapati <vansh.prajapati1104@gmail.com>
2024-04-06 05:23:53 -0400

    like unlike from the database as well

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -5,6 +5,49 @@ const endBox = document.getElementById('end-box')
 
 let visible = 3
 
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = () => {
+    const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnlikeForms.forEach(form => form.addEventListener('submit', e => {
+        e.preventDefault()
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: 'POST',
+            url: "/like-unlike/",
+            data: {
+                'csrfmiddlewaretoken' : csrftoken,
+                'pk':clickedId,
+            },
+            success: function(response){
+                console.log(response)
+                clickedBtn.textContent = response.liked ? `Unlike(${response.count})`: `Like(${response.count})`
+            },
+            error: function(error){
+                console.log(error)
+            }
+        })
+    }))
+    console.log(likeUnlikeForms)
+}
+
 const get_data = () => {
   endBox.classList.add('not-visible')
   $.ajax({
@@ -27,7 +70,11 @@ const get_data = () => {
             <div class="card-footer">
               <div class="row">
                 <div class="col-2"><a href="#" class="btn btn-primary">Details...</a></div>
-                <div class="col-2"><a href="#" class="btn btn-primary">${el.liked ? `Unlike(${el.count})`: `Like(${el.count})`}</a></div>
+                <div class="col-2">
+                <form class='like-unlike-forms' data-form-id="${el.id}">
+                    <button href="#" class="btn btn-primary" id="like-unlike-${el.id}"> ${el.liked ? `Unlike(${el.count})`: `Like(${el.count})`}</button>
+                </form>    
+                </div>
               </div>
             </div>
           </div>
@@ -38,6 +85,7 @@ const get_data = () => {
         } else if (visible < resp.size) {
           endBox.classList.remove('not-visible')
         }
+        likeUnlikePosts()
       }, 100)
     },
     error: err => {
