bd6109f0815c13134accf016925ecd8f9d5821b4
Vansh Prajapati <vansh.prajapati1104@gmail.com>
2024-04-06 04:17:40 -0400

    updated posts mainly the UI and logic for the js file

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,5 +1,55 @@
-console.log("...Hello Worild....");
+const postBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner')
+const loadBtn = document.getElementById('load-btn')
+const endBox = document.getElementById('end-box')
 
-const helloWorldBox = document.getElementById('hello-world')
+let visible = 3
 
-helloWorldBox.innerHTML = 'hello <b>World</b>..:).'
+const get_data = () => {
+  endBox.classList.add('not-visible')
+  $.ajax({
+    type: 'GET',
+    url: `/api/posts/${visible}/`,
+    success: resp => {
+      console.log(resp)
+      // Give some time for the spinner to appear.
+      setTimeout(() => {
+        spinnerBox.classList.add('not-visible')
+        // postBox.innerHTML = ''
+        resp.data.forEach( el => {
+          postBox.innerHTML += `
+            <div class="card mb-2">
+            <!-- <img src="..." class="card-img-top" alt="..."> --!>
+            <div class="card-body">
+              <h5 class="card-title">${el.title}</h5>
+              <p class="card-text">${el.body}</p>
+            </div>
+            <div class="card-footer">
+              <div class="row">
+                <div class="col-2"><a href="#" class="btn btn-primary">Details...</a></div>
+                <div class="col-2"><a href="#" class="btn btn-primary">Like</a></div>
+              </div>
+            </div>
+          </div>
+          `
+        })
+        if (resp.size == 0) {
+          endBox.innerHTML = 'No posts added yet...'
+        } else if (visible < resp.size) {
+          endBox.classList.remove('not-visible')
+        }
+      }, 100)
+    },
+    error: err => {
+      console.error(err)
+    }
+  })
+}
+
+loadBtn.addEventListener('click', () => {
+  spinnerBox.classList.remove('not-visible')
+  visible += 3
+  get_data()
+})
+
+get_data()
--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -6,3 +6,21 @@ from .models import Post
 def post_list_and_create(request):
   qs = Post.objects.all()
   return render(request, 'posts/main.html', {'qs':qs})
+
+def load_post_data_view(request, num_posts):
+  def post_as_dict(post:Post):
+    d = post.as_dict()
+    d['liked'] = request.user in post.liked.all()
+    return d
+
+  qs = Post.objects.all()
+  visible = 3
+  upper = num_posts
+  lower = num_posts - visible
+  size = qs.count()
+
+  data = [post_as_dict(o) for o in qs]
+  return JsonResponse({'data':data[lower:upper], 'size':size, 'lower':lower, 'upper':upper})
+
+def hello_world_view(request):
+  return JsonResponse({'text':'hello ajax'})
