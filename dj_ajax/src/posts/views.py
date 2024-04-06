a356a7b9fb63eb09816d59de5f930fafe5943f70
Vansh Prajapati <vansh.prajapati1104@gmail.com>
2024-04-06 15:47:20 -0400

    setup for the spiner is done for the post detail

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -63,7 +63,16 @@ def load_post_data_view(request, num_posts):
     
     return JsonResponse({'data':data[lower:upper], 'size':size})
 
-
+def post_detail_data_view(request,pk):
+    obj = Post.objects.get(pk=pk)
+    data = {
+        'id':obj.id,
+        'title': obj.title,
+        'body': obj.body,
+        'author': obj.author.user.username,
+        'logged_in': request.user.username,
+    }
+    return JsonResponse({'data': data})
 
 def like_unlike_post(request):
     if is_ajax(request=request):
@@ -76,7 +85,3 @@ def like_unlike_post(request):
             liked = True
             obj.liked.add(request.user)
         return JsonResponse({'liked': liked, 'count':obj.like_count})
-
-
-def hello_world_view(request):
-    return JsonResponse({'text':'hello ajax'})
